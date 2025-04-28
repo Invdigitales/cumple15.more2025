d582e5cc715fdabb631806f495205da3a5cb0797
Invdigitales <97065304+Invdigitales@users.noreply.github.com>
2025-04-28 13:12:52 -0300

    cinco

--- main.js
+++ main.js
@@ -71,7 +71,7 @@ function diaEventoAhora () {
       icon: 'success',
       html:
         'INGRESÁ' +
-        '<iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3343.448143163336!2d-68.478574624325!3d-33.07099277354218!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMzPCsDA0JzE1LjYiUyA2OMKwMjgnMzMuNiJX!5e0!3m2!1ses!2sar!4v1723587557956!5m2!1ses!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>' +
+        '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3277.4375448401706!2d-58.202844999999996!3d-34.7697588!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95a328b188623d6b%3A0xfb54eccc2e4f5dcb!2sRecepciones%20San%20Francisco!5e0!3m2!1ses-419!2sar!4v1745856681025!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>' +
         'Podes ampliar el mapa',
         
         
